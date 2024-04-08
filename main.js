a8436e5431830df9ba36e868715d9aa5c40a1a3d
humannocturnal <syaputrar374@gmail.com>
2024-04-08 14:06:49 +0700

    Praktikum

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("video").addEventListener("click", function() {
+    alert("Ini Video Kana Boon");
+});
+
+document.getElementById("audio").addEventListener("click", function() {
+    alert("Ini Audio Kana Boon");
+});
+
+document.getElementById("gambar").addEventListener("click", function() {
+    alert("Ini Gambar Kana Boon");
+});
